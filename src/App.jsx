8e1727ee443ac789cd4acc0527957064a793f6c8
AlexisKcc8 <CodeKJS863@gmail.com>
2024-07-03 13:38:44 -0600

    remenber update the description when end project

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,15 @@ function App() {
     <section>
       <header className="header-section-app">
         <h1 className="title">Bienvenido a MapKml🗺️</h1>
+        <h3>Actualizar la descripcion cuando se termine el proyecto</h3>
         <p className="description">
           Este proyecto es una aplicación web desarrollada con React que permite
           a los usuarios cargar archivos KML (Keyhole Markup Language),
           visualizar su contenido geoespacial en un mapa interactivo y exportar
           los datos geoespaciales a un script SQL. Utiliza react-dropzone para
-          crear una zona drag & drop, Leaflet para la visualización de mapas y
-          la biblioteca leaflet-kml para manejar y renderizar archivos KML en el
-          mapa.
+          crear una zona drag & drop, Leaflet para la visualización de mapas, la
+          biblioteca React Leaflet para proporcionar enlaces entre React y
+          Leaflet.
         </p>
       </header>
       <main className="main-content">
