822855a1b34bf63eb3ecff93c10823c579d749a9
AlexisKcc8 <CodeKJS863@gmail.com>
2024-07-03 13:29:41 -0600

    clear readers in kmlMap and add tooltips in map

--- src/components/KmlMap/KmlMap.jsx
+++ src/components/KmlMap/KmlMap.jsx
@@ -1,4 +1,10 @@
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  Popup,
+  Tooltip,
+} from "react-leaflet";
 import { kml } from "@tmcw/togeojson";
 import { saveAs } from "file-saver";
 import "leaflet/dist/leaflet.css";
@@ -11,31 +17,43 @@ export const KmlMap = () => {
   const [geojsonData, setGeojsonData] = useState([]);
   const newGeojsonData = [];
   useEffect(() => {
-    if (validateFile.length > 0) {
-      console.log(validateFile);
-      validateFile.forEach((kmlFile) => {
-        const reader = new FileReader();
-        reader.onload = (e) => {
+    if (validateFile.length < 0) return;
+
+    const readers = [];
+    let isMounted = true;
+
+    validateFile.forEach((kmlFile) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (isMounted) {
           const parser = new DOMParser();
           const kmlDoc = parser.parseFromString(
             e.target.result,
             "application/xml"
           );
           const geoJson = kml(kmlDoc);
-          console.log(geoJson);
           newGeojsonData.push(geoJson);
           if (newGeojsonData.length === validateFile.length) {
             setGeojsonData(newGeojsonData);
           }
-        };
-        // reader.readAsText(files[i]);
-        reader.readAsText(kmlFile);
-      });
-    }
+        }
+      };
 
-    // return () => { checar
-    //   second
-    // }
+      reader.onerror = (error) => {
+        if (isMounted) {
+          console.error("Error reading file:", error);
+        }
+      };
+
+      readers.push(reader);
+      reader.readAsText(kmlFile);
+    });
+
+    return () => {
+      isMounted = false;
+      readers.forEach((reader) => reader.abort());
+      console.log("All readers aborted and cleaned up");
+    };
   }, []);
 
   return (
@@ -51,14 +69,24 @@ export const KmlMap = () => {
           </li>
         ))}
       </ul>
+
       <MapContainer
         center={[0, 0]}
         zoom={2}
         style={{ height: "30rem", width: "100%" }}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
         {geojsonData.map((geoJson, index) => (
-          <GeoJSON key={index} data={geoJson} />
+          <GeoJSON key={index} data={geoJson}>
+            <Tooltip direction="bottom" offset={[0, 10]} opacity={1} permanent>
+              {geoJson.features[0].properties.name}
+            </Tooltip>
+          </GeoJSON>
         ))}
       </MapContainer>
 
